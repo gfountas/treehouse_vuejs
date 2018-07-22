6c2dd2c3759ed63a173681b06bf1b42734ea357a
gfountas <fgiorgosf@gmail.com>
2018-07-22 13:25:07 +0100

    Video 5

--- app.js
+++ app.js
@@ -4,4 +4,13 @@ const helloVue = new Vue({
         title: "Hello World from Vue!!!",
         message: "This is a message"
     }
-})
+});
+
+const example = new Vue({
+    el: '#example',
+    data: {
+        title: "Hello",
+        name: "Dollie",
+        message: "This is a message"
+    }
+});
