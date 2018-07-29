959a250e750e8ce7238bf9333851a1cde39671c3
George F <fgiorgosf@gmail.com>
2018-07-29 20:01:50 +0100

    Vue.js complete

--- README.md
+++
@@ -1,1 +0,0 @@
-# treehouse_vuejs
--- js-vue-flashcards/app.js
+++ js-vue-flashcards/app.js
@@ -1,49 +1,61 @@
 const cards = [
     {
-      front: 'The "First Computer Programmer"',
-      back: 'Ada Lovelace',
-      flipped: false,
+        front: 'The "First Computer Programmer"',
+        back: 'Ada Lovelace',
+        flipped: false,
     },
     {
-      front: 'Invented the "Clarke Calculator"',
-      back: 'Edith Clarke',
-      flipped: false,
-  
-    },
-    {
-      front: 'Famous World War II Enigma code breaker',
-      back: 'Alan Turing',
-      flipped: false,
-    },
-    {
-      front: 'Created satellite orbit analyzation software for NASA',
-      back: 'Dr. Evelyn Boyd Granville',
-      flipped: false,
-    },
-  ]; 
+        front: 'Invented the "Clarke Calculator"',
+        back: 'Edith Clarke',
+        flipped: false,
 
-  new Vue({
+    },
+    {
+        front: 'Famous World War II Enigma code breaker',
+        back: 'Alan Turing',
+        flipped: false,
+    },
+    {
+        front: 'Created satellite orbit analyzation software for NASA',
+        back: 'Dr. Evelyn Boyd Granville',
+        flipped: false,
+    },
+];
+
+new Vue({
     el: '#flashcard-app',
     data: {
-      cards: cards,
-      newFront: "",
-      newBack: ""
+        cards: cards,
+        newFront: "",
+        newBack: "",
+        error: false
     },
     methods: {
 
-      flipCard: function(card) {
-        card.flipped = !card.flipped;
-      },
-      removeCard: function(index){
-        cards.splice(index,1);
-      },
-      addNewCard: function() {
-        this.cards.push({
-          front: this.newFront,
-          back: this.newBack,
-          flipped: false
-        })
-      }
+        flipCard: function (card) {
+            card.flipped = !card.flipped;
+        },
+        removeCard: function (index) {
+            cards.splice(index, 1);
+        },
+        addNewCard: function () {
+
+            if (this.newFront!=='' && this.newBack!==''){
+                this.cards.push({
+                    front: this.newFront,
+                    back: this.newBack,
+                    flipped: false
+                });
+                this.newFront='';
+                this.newBack='';
+                this.error=false;
+            }
+            else {
+                this.error=true;
+            }
+
+
+        }
 
     }
-  });
+});
